c6c286b9aecaa2a1b7683bf6cd352fc8f66d7aea
alineanascimento <aline.nascimento@bsatech.io>
2025-09-29 15:33:34 -0300

    .

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-
-print("""
+def exibir_nome_do_progrma():
+    
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -8,12 +9,31 @@ print("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurantes")
-print("3. Ativar restaurante")
-print("4. Sair\n")
+    
+def exibir_opcao():
+   
+    print("Finalizando o app...\n")
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurantes")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
 
-opcao_escolhida = int(input("Escolha uma opção: "))
+def finalizar_app():
+    os.system("cls")
+    print("Finalizando o app...\n")
+
+def escolher_opcoes():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    # print(f"Você escolheu a opção {opcao_escolhida}")
+
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante...")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes...")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante...")
+    else:
+        finalizar_app()
 # print(f"Você escolheu a opção {opcao_escolhida}")
 
 # exercicio
@@ -31,19 +51,11 @@ print("A\nL\nU\nR\nA")
 pi = 3.14159
 print(f"O valor arredondado de pi é: {pi:.2f}")"""
 
-def finalizar_app():
-    os.system("cls")
-    print("Finalizando o app...\n")
 
-
-
-
-
-if opcao_escolhida == 1:
-    print("Cadastrar restaurante...")
-elif opcao_escolhida == 2:
-    print("Listar restaurantes...")
-elif opcao_escolhida == 3:
-    print("Ativar restaurante...")
-else:
-    finalizar_app()
+def main():
+    exibir_nome_do_progrma()
+    exibir_opcao()
+    escolher_opcoes()
+    
+if __name__ == "__main__":
+    main()
---
+++ exercicioos_02.py
@@ -0,0 +1,36 @@
+#1
+
+numero = int(input("Insira um número: ")))
+if numero % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
+    
+#2
+idade = int(input("Insira sua idade: "))
+if   0 <= idade < 12:
+    print("Criança")
+elif 13 <= idade < 18:
+    print("Adolescente")
+else:
+    print("Adulto")
+
+#3
+nome_usuario = input("Insira seu nome de usuário: ")
+senha_usuario = int(input("Insira sua senha: "))
+if nome_usuario == "Aline" and senha_usuario == "1234":
+    print("Acesso permitido")
+
+#4
+coordenadas = int(input("Insira a coordenada x: ")), int(input("Insira a coordenada y: "))
+if coordenadas[0] > 0 and coordenadas[1] > 0:
+    print("Primeiro quadrante")
+elif coordenadas[0] < 0 and coordenadas[1] > 0:
+    print("Segundo quadrante")
+elif coordenadas[0] < 0 and coordenadas[1] < 0:
+    print("Terceiro quadrante")
+elif coordenadas[0] > 0 and coordenadas[1] < 0:
+    print("Quarto quadrante")
+else:
+    print("Origem")
+    
