e7c7aebd1e5dd08ee12fedc89f30ff60fae1a316
Aline Nascimento | BSATech <aline.nascimento@BSATech.LOCAL>
2025-10-07 17:33:27 -0300

    feat: create product table and list products in loja.db

---
+++ Curso-4/Modulo-5/Desafio/loja.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+
+def conectar():
+    conn = sqlite3.connect("loja.db")
+    return conn 
+
+def criar_tabela_produtos():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+            CREATE TABLE IF NOT EXISTS produtos(
+                id INTEGER PRIMARY KEY,
+                nome TEXT,
+                preco FLOAT
+            
+            )
+
+        """
+    )
+    conn.commit()
+    conn.close()
+
+
+def inserir_produtos(nome, preco):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+            INSERT INTO produtos (nome, preco) \
+            VALUES (?, ?)
+
+        """, (nome, preco)
+    )
+    conn.commit()
+    conn.close()
+
+def listar_produtos():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+            SELECT * FROM produtos
+        """
+    )
+    produtos = cursor.fetchall()
+    for produto in produtos:
+        print(produto)
+
+    conn.commit()
+    conn.close()
+
+# para executar exec(open("loja.py").read())
