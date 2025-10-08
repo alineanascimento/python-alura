01a6b2ccc7bfcb5506969ba73fe37377ba778e5f
alineanascimento <alineacioly@hotmail.com>
2025-10-08 09:39:54 -0300

    chore: reorganize folders by separating exercises from challenge files

---
+++ Curso-4/Modulo-1/Desafio/dict.py
@@ -0,0 +1,4 @@
+filmes = {"Star wars": "20/10", "Harry Potter": "30/09", "A bela e a fera": "01/01"}
+
+for filme in filmes:
+    print(f'{filme} - {filmes[filme]}')
---
+++ Curso-4/Modulo-3/Exercicios/db_sql.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+conn = sqlite3.connect("escola.db")
+
+cursor = conn.cursor()
+
+cursor.execute(
+    """
+        CREATE TABLE IF NOT EXISTS estudantes(
+            id INTEGER PRIMARY KEY,
+            nome TEXT,
+            idade INTEGER
+        
+
+        )
+
+    """
+)
+
+cursor.execute(
+    "INSERT INTO estudantes (nome, idade)\
+     VALUES (?, ?)", ("João", 20)
+)
+
+conn.commit()
+
+cursor.execute("SELECT * FROM estudantes")
+
+print(cursor.fetchall())
+
+conn.close()
